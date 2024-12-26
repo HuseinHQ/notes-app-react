8b52bcb8c2b471744b110c2663695ced9942f196
HuseinHQ <mhusein463@gmail.com>
2024-12-26 19:42:27 +0700

    Update Vite configuration base path for submission project

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/discussion-forum-react/',
+  base: '/submission-belajar-membuat-aplikasi-web-dengan-react/',
 });
